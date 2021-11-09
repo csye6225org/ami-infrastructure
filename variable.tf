4a8a66f44aae38b36ab79fded7b9b8177102983f
Varad Lata Ratnakar Desai <varad.desai.rl@gmail.com>
2021-11-09 11:52:50 -0500

    Policy changes

--- variable.tf
+++ variable.tf
@@ -2,3 +2,11 @@ variable "aws_iam_user" {
   type        = string
   description = "User for generating ami"
 }
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+}
+variable "aws_account_id" {
+  type        = string
+  description = "AWS account id"
+}
